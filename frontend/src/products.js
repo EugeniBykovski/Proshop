1c403afbe52ca91b1e34145e87b83ed21c96fcc6
EugeniBykovski <bykovskieugeni@mail.ru>
2022-02-17 11:33:51 +0300

    created Priduct page, updated link navbar, updated templates

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -8,7 +8,7 @@ const products = [
     brand: 'Apple',
     category: 'Electronics',
     price: 89.99,
-    countInStock: 10,
+    countInStock: 0,
     rating: 4.5,
     numReviews: 12,
   },
@@ -22,7 +22,7 @@ const products = [
     category: 'Electronics',
     price: 599.99,
     countInStock: 7,
-    rating: 4.0,
+    rating: 4.8,
     numReviews: 8,
   },
   {
