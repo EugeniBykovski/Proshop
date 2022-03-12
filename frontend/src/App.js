e02883a58e28f40ee886f483a47454daf947dd69
EugeniBykovski <bykovskieugeni@mail.ru>
2022-03-12 14:55:12 +0300

    added backend and frontend user authentication and profile, added frontend checkout process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,12 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen'; 
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const App = () => {
   return (
@@ -15,9 +21,15 @@ const App = () => {
 
       <main className='py-3'>
         <Container>
-          <Route path='/' component={ HomeScreen } exact />
+          <Route path='/shipping' component={ ShippingScreen } />
+          <Route path='/payment' component={ PaymentScreen } />
+          <Route path='/placeorder' component={ PlaceOrderScreen } />
+          <Route path='/login' component={ LoginScreen } />
+          <Route path='/register' component={ RegisterScreen } />
+          <Route path='/profile' component={ ProfileScreen } />
           <Route path='/product/:id' component={ ProductScreen } />
           <Route path='/cart/:id?' component={ CartScreen } />
+          <Route path='/' component={ HomeScreen } exact />
         </Container>
       </main>
 
